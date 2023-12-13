1a6131ba72fc2e652119580a2e6be012a52ff2cf
Taras Kovalyk <tarik2454@gmail.com>
2023-12-13 18:31:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-project-template/',
+  base: '/website-LearnLingo/',
 });
